9070b06a0bdfd49fa57548616d97cf6832e7bdb5
amankumbhalwar09 <amankumbhalwar09@gmail.com>
2024-09-16 10:26:54 +0000

    portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  darkMode: 'class', // Enable dark mode using the 'class' strategy
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
